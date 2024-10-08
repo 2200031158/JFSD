acdbe3f278b5835a51910748177d3fbb988e7ebf
2200031158 <vakkalankaashritha@gmail.com>
2024-10-08 08:06:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import AdminAuth from './components/AdminAuth';
+import UserAuth from './components/UserAuth';
+import About from './components/About';
+import Help from './components/Help';
+import Admin from './components/Admin'; // Admin module
+import User from './components/User';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Main Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/admin-login" element={<AdminAuth />} />
+          <Route path="/user-login" element={<UserAuth />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/help" element={<Help />} />
+
+          {/* Admin Routes */}
+          <Route path="/admin/*" element={<Admin />} /> {/* Admin has nested routes */}
+          <Route path="/user" element={<User />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
